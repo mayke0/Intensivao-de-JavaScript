da68b1211de6828e0da70ed1a44af886ed1fd6ee
mayke0 <mayke2_0@yahoo.com>
2023-10-06 18:41:22 -0300

    atualizacao

--- Intensivao-de-JavaScript/main.js
+++ Intensivao-de-JavaScript/main.js
@@ -1,10 +1,14 @@
 import { renderizarCatalogo } from "./src/cartaoProduto";
+import { inicializarFiltros } from "./src/filtrosCatalogo";
 import {
   atualizarPrecoCarrinho,
   inicializarCarrinho,
   renderizarProdutoCarrinho,
 } from "./src/menuCarrinho";
+
 renderizarCatalogo();
 inicializarCarrinho();
 renderizarProdutoCarrinho();
 atualizarPrecoCarrinho();
+inicializarFiltros();
+inicializarFiltros;
